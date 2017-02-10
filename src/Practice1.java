69a5bd4241d81c8d0783cb2910468e95b62afaa8
saurabhDuzumaki <srbhkulkarni13@gmail.com>
2017-02-10 13:57:49 +0530

    CodeChef : Java is in my blood
    counts the total number of words written in camel case in each word
    (e.g. heyHelloWORLD = 7)

---
+++ src/Practice1.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner; 
+
+public class Practice1 {
+	public static void main(String[] args) {
+		
+		int size = 0;
+		//int totalCount = 0;
+		Scanner sc = new Scanner(System.in);
+		size = sc.nextInt();
+		ArrayList<String> al = new ArrayList<>();
+		int i = 0;
+		//String s = "";
+		
+		while(i<size){
+			al.add(sc.next());
+			i++;
+		}
+		sc.close();
+		
+		//al.stream().chars().filter(item -> Character.isUpperCase(item).count());
+		//filter(al, (item) -> item.chars().filter(character -> Character.isUpperCase(character)));
+		
+		for(int j=0;j<size;j++){
+			
+			long count=al.get(j).chars().filter(character -> Character.isUpperCase(character)).count();
+			
+			/*s=al.get(j);
+			totalCount=1;*/
+			
+			/*for(int x=0;x<s.length();x++){
+				int ascii = (int) s.charAt(x);
+				if(ascii>64 && ascii<91){
+					totalCount++;
+				}
+			}*/
+			
+			System.out.println(count+1);
+		}
+	}
+}
