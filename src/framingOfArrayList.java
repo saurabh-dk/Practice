c1a3d6c2b3e9c6edb76c0234c107e34e5f381a15
saurabhDuzumaki <srbhkulkarni13@gmail.com>
2017-02-10 13:51:43 +0530

    Framing of input
    Taking input, storing it in ArrayList and framing it withing a frame of
    stars (with and without lambda functions).

---
+++ src/framingOfArrayList.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class framingOfArrayList {
+
+	static int maxSize;
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> al = new ArrayList<>();
+		String s = "";
+		int size = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter \"stop\" to finish ==>");
+		
+		while(!"stop".equalsIgnoreCase(s)){
+			
+			s = sc.next();
+			al.add(s);
+			
+			size++;
+			
+		}
+		
+		sc.close();
+		al.remove(size-1);
+		al.trimToSize();
+		
+		String longest = al.stream().max(Comparator.comparing(str -> str.length())).get();
+		
+		/*String longestString = al.get(0);
+		for (String temp : al){
+	    
+			if (temp.length() > longestString.length()){
+	    
+				longestString = temp;
+	       
+			}
+		
+		}*/
+		
+		maxSize = longest.length();
+		
+		printStars();
+		
+		System.out.print("\n");
+		
+		al.forEach(
+			str -> {System.out.print("* "+str);
+			
+						for(int i=str.length();i<=maxSize;i++){
+							System.out.print(" ");
+						}
+					System.out.println("*");
+					}
+				);
+		
+		/*for(int x=0;x<al.size();x++){
+		
+			System.out.print("* "+al.get(x));
+			
+			for(int i=al.get(x).length();i<=maxSize;i++){
+			
+				System.out.print(" ");
+			 
+			} 
+			
+			System.out.println("*");
+		
+		}*/
+		  
+		printStars();
+	
+	}
+
+	private static void printStars() {
+
+		for(int i=0;i<maxSize+4;i++){
+			
+			System.out.print("*");
+		
+		}
+	}
+
+}
